c81cc295e0555068ad25a63b5d608860aa6c312d
Pavan Nittur <nittur.pavan@gmail.com>
2018-04-15 10:47:21 +0530

    Added modules
    Added summary , preprocessing and feature extraction modules.

---
+++ Preprocessing/svm_text_class.py
@@ -0,0 +1,44 @@
+import numpy as np
+from sklearn import svm
+import csv
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+data = np.array(list(csv.reader(open("noise_nonoise.csv"))))
+f = open("converteddata.txt","r")
+
+act_target = data[1:,0]
+text = data[1:,1]
+
+noise_t = np.where(act_target=="noise", 1,-1)
+
+#target is ready. We need to restructure the text now into features.
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(text)
+print(count_vect.get_feature_names())
+
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+print(X_train_tfidf)
+
+
+#Train
+clf = svm.SVC(kernel='linear')
+clf.fit(X_train_tfidf,noise_t)
+
+#New Instance
+test_text=[]
+if f.mode == "r":
+	f1 = f.readlines()
+	for x in f1:
+		test_text.append(x)
+		
+
+X_new_counts = count_vect.transform(test_text)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+
+for i in range(3):
+    print("Text:", test_text[i], "...Classification:","not noise\n" if predicted[i] == -1 else "noise\n")
---
+++ Summa/summa/textrank.py
@@ -0,0 +1,81 @@
+import sys, getopt
+
+from .summarizer import summarize
+from .keywords import keywords
+
+# Types of summarization
+SENTENCE = 0
+WORD = 1
+
+
+def exit_with_error(err):
+    print("Error: " + str(err))
+    usage()
+    sys.exit(2)
+
+def get_arguments():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "t:s:r:w:h", ["text=", "summary=", "ratio=", "words=", "help"])
+    except getopt.GetoptError as err:
+        exit_with_error(err)
+
+    path = None
+    summarize_by = SENTENCE
+    ratio = 0.2
+    words = None
+    for o, a in opts:
+        if o in ("-t", "--text"):
+            path = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-s", "--summary"):
+            summarize_by = int(a)
+        elif o in ("-w", "--words"):
+            words = int(a)
+        elif o in ("-r", "--ratio"):
+            ratio = float(a)
+        else:
+            assert False, "unhandled option"
+
+    if path is None:
+        exit_with_error("-t option is required.")
+
+    return path, summarize_by, ratio, words
+
+
+help_text = """Usage: textrank -t FILE
+-s UNIT, --summary=UNIT:
+\tType of unit to summarize: sentence (0) or word (1). Default value: 0
+\t0: Sentence. 1: Word
+-t FILE, --text=FILE:
+\tPATH to text to summarize
+-r RATIO, --ratio=RATIO:
+\tFloat number (0,1] that defines the length of the summary. It's a proportion of the original text. Default value: 0.2.
+-w WORDS, --words=WORDS:
+\tNumber to limit the length of the summary. The length option is ignored if the word limit is set.
+-h, --help:
+\tprints this help
+"""
+def usage():
+    print(help_text)
+
+
+def textrank(text, summarize_by=SENTENCE, ratio=0.2, words=None):
+    if summarize_by == SENTENCE:
+        return summarize(text, ratio, words)
+    else:
+        return keywords(text, ratio, words)
+
+
+def main():
+    path, summarize_by, ratio, words = get_arguments()
+
+    with open(path) as file:
+        text = file.read()
+
+    print(textrank(text, summarize_by, ratio, words))
+
+
+if __name__ == "__main__":
+    main()
